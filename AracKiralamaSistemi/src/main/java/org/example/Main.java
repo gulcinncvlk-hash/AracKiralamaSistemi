464ccff6d44edecd57054896dcdc8ce2e31833a8
Gülcin Civelek <gulcinncvlk@gmail.com>
2025-12-02 22:55:52 +0300

    "Main sınıfında test yapıldı:Hesaplamalar doğru çalışıyor"

--- AracKiralamaSistemi/src/main/java/org/example/Main.java
+++ AracKiralamaSistemi/src/main/java/org/example/Main.java
@@ -1,17 +1,26 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("--- ARAÇ KİRALAMA SİSTEMİ TESTİ ---\n");
+
+        // 1. Benzinli Araba Üretelim (Günlük 1000 TL)
+        Araba benzinli = new BenzinliAraba("Fiat", "Egea", 1000);
+
+        System.out.println("Araç: " + benzinli.getMarka() + " " + benzinli.getModel());
+        System.out.println("Yakıt Türü: Benzinli");
+        // 3 Günlük kiralayalım (1000 * 3 = 3000 TL olmalı)
+        System.out.println("3 Günlük Ücret: " + benzinli.calculateRentalFee(3) + " TL");
+
+        System.out.println("\n----------------------------------\n");
+
+        // 2. Elektrikli Araba Üretelim (Günlük 2000 TL)
+        Araba elektrikli = new ElektrikliAraba("Tesla", "Model Y", 2000);
+
+        System.out.println("Araç: " + elektrikli.getMarka() + " " + elektrikli.getModel());
+        System.out.println("Yakıt Türü: Elektrikli");
+        // 3 Günlük kiralayalım. Normalde 6000 TL ama %20 indirimle 4800 TL olmalı!
+        System.out.println("3 Günlük Ücret (İndirimli): " + elektrikli.calculateRentalFee(3) +"TL");
     }
 }
